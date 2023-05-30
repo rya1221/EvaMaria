6aaeccace3ec638c3d1c95b9969890c6b57f4625
rya1221 <91537646+rya1221@users.noreply.github.com>
2023-05-30 13:05:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = environ.get('AUTH_CHANNEL')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://arya:arya@cluster0.fmoiykc.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://arya:arya@cluster0.95aiabp.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Digambar Singh")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Anurag_files')
 
