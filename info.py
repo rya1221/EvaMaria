633f7551bef43905eb70a004b3685befe1b4ee93
rya1221 <91537646+rya1221@users.noreply.github.com>
2023-05-30 11:24:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Digambar Singh")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Anurag_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1929859353'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1980976777'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'friends_forever_chatting_world')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'True')), False)
 IMDB = is_enabled((environ.get('IMDB', 'False')), True)
